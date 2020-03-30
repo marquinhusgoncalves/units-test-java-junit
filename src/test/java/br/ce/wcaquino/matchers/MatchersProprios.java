73fdd730757b624cc83f108acc7061330241a201
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-03-29 22:59:40 -0300

    chore: cchallenge

--- src/test/java/br/ce/wcaquino/matchers/MatchersProprios.java
+++ src/test/java/br/ce/wcaquino/matchers/MatchersProprios.java
@@ -11,4 +11,12 @@ public class MatchersProprios {
 	public static DiaSemanaMatcher caiNumaSegunda(){
 		return new DiaSemanaMatcher(Calendar.MONDAY);
 	}
+	
+	public static DataDiferencaDiasMatcher ehHojeComDiferencaDias(Integer qtdDias) {
+		return new DataDiferencaDiasMatcher(qtdDias);
+	}
+
+	public static DataDiferencaDiasMatcher ehHoje() {
+		return new DataDiferencaDiasMatcher(0);
+	}
 }
